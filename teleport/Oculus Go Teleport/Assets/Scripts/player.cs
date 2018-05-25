62a4681b171a3a34cc0a6a1d99aba6f1f37164d9
Nicholas Liang <ssss021418@gmail.com>
2018-05-24 18:33:26 -0700

    Teleport Example finished

---
+++ teleport/Oculus Go Teleport/Assets/Scripts/player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+			anim = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+			if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger)) {
+				anim.Play("WAIT04", -1, 0f);
+			}
+	}
+}
